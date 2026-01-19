c59f6ae5da8abbcb74063f74ea17536cf58d7cae
keani-julian <julian_keani01@student.smc.edu>
2026-01-18 16:55:15 -0800

    Add isEvenOrOdd() function to journal

--- journal.js
+++ journal.js
@@ -2,3 +2,27 @@
 function greet(name) {
   return "Hello, " + name + "!";
 }
+function isEvenOrOdd(number) {
+  if (number % 2 === 0) {
+    return "even";
+  }
+  return "odd";
+}
+function convertMinutesToSeconds(minutes) {
+  return minutes * 60;
+}
+function canRead(age) {
+  return age >= "3";
+}
+function countDown(number) {
+  if (number === 0) {
+    return "Done!";
+  }
+  return number + " " + countDown(number - 1);
+}
+function maxOfTwo(a, b) {
+  if (a > b) {
+    return a;
+  }
+  return b;
+}
